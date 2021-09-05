b2db4a53f93d40c090f225eed7ce14ddd41e23e1
zMarkoz <88552612+zMarkoz@users.noreply.github.com>
2021-09-05 19:15:02 +0200

    Add files via upload

---
+++ ITBC/Aplikacije/NutriApp/Drink.java
@@ -0,0 +1,7 @@
+package NutriApp;
+
+public class Drink extends Nutriment{
+    public Drink(String company, String name, double weight, double kcal, double proteins, double carb, double fats, double fiber) {
+        super(company, name, weight, kcal, proteins, carb, fats, fiber);
+    }
+}
---
+++ ITBC/Aplikacije/NutriApp/Food.java
@@ -0,0 +1,8 @@
+package NutriApp;
+
+public class Food extends Nutriment{
+    public Food(String company, String name, double weight, double kcal, double proteins, double carb, double fats, double fiber) {
+        super(company, name, weight, kcal, proteins, carb, fats, fiber);
+    }
+
+}
